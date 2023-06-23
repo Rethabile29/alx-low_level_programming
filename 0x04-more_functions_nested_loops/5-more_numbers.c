403cbc12435b6b125503e8107f93cff711cddca1
root <rtlale29@gmail.com>
2023-06-23 13:25:02 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "main,h"
+#include "main.h"
 
 
 /**
@@ -12,7 +12,7 @@ void more_numbers(void)
 
 	int x, y;
 
-	for (x = 0; x < 18; x++)
+	for (x = 0; x < 10; x++)
 	{
 	for (y = 0; y <= 14; y++)
 	{
